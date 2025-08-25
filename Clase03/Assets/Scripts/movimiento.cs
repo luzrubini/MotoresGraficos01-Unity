0c54acf20c82f491527a2054d0e84e80aab75d5b
Luz Rubini <luzrubini@hotmail.com>
2025-08-25 20:24:15 -0300

    movimiento + camara
    Se agrego la funcionalidad de movimiento en la capsula que representa al jugador, se puede rotar la camara de izquierda a derecha.
    bugfix: de arriba a abajo no funciona, la camara decae mas rapido de lo que deberia

--- Clase03/Assets/Scripts/movimiento.cs
+++ Clase03/Assets/Scripts/movimiento.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class movimiento : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movimiento")]
+    public float speed = 30f;
+    public float jumpForce = 7f;
+
+    [Header("Mouse Look")]
+    public float mouseSensitivity = 400f;
+    float xRotation = 0f;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked; 
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        // --- Movimiento ---
+        float moveX = Input.GetAxis("Horizontal"); 
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        Vector3 newPos = transform.position + move * speed * Time.deltaTime;
+
+        rb.MovePosition(newPos);
+
+        // --- Rotación con mouse ---
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
         
+        transform.Rotate(Vector3.up * mouseX);
+
+        
+        //xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f);
     }
 }
